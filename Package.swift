95baf389da0944d5da53339c380c43c7c79f21dc
Brendan Thompson <github@blt.is>
2023-04-28 10:54:58 +1000

    chore: remove tests

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "ExtensionsKit",
             dependencies: []),
-        .testTarget(
-            name: "ExtensionsKitTests",
-            dependencies: ["ExtensionsKit"]),
     ]
 )
