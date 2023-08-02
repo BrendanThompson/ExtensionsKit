2d641e0c562c81d2be402d0146be953c5e59ffcd
Brendan Thompson <github@blt.is>
2023-08-02 12:24:08 +1000

    fix: add available and updated deprecated method

--- Sources/Color.swift
+++ Sources/Color.swift
@@ -4,7 +4,7 @@ import UIKit
 
 public typealias Kelvin = Double
 
-@available(iOS 13, tvOS 13, *)
+@available(iOS 13, tvOS 13, macOS 13, *)
 public extension Color {
     init(temperature: Kelvin) {
         let components = componentsForColorTemperature(temperature: temperature)
@@ -14,54 +14,45 @@ public extension Color {
 
 /// Color can now be stored in UserDefaults
 ///
-/// Credit: https://medium.com/geekculture/using-appstorage-with-swiftui-colors-and-some-nskeyedarchiver-magic-a38038383c5e
-public extension Color: RawRepresentable {
-
+/// Inspired by: https://medium.com/geekculture/using-appstorage-with-swiftui-colors-and-some-nskeyedarchiver-magic-a38038383c5e
+@available(iOS 14, tvOS 13, macOS 13, *)
+extension Color: RawRepresentable {
     public init?(rawValue: String) {
-
-        guard let data = Data(base64Encoded: rawValue) else{
+        guard let data = Data(base64Encoded: rawValue) else {
             self = .black
             return
         }
-
-        do{
-            let color = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor ?? .black
+        do {
+            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data) ?? .black
             self = Color(color)
-        }catch{
+        } catch {
             self = .black
         }
-
     }
 
     public var rawValue: String {
-
-        do{
+        do {
             let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
             return data.base64EncodedString()
 
-        }catch{
-
+        } catch {
             return ""
-
         }
-
     }
-
 }
 
 // Algorithm taken from Tanner Helland's post: http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
 func componentsForColorTemperature(temperature: Kelvin) -> (red: Double, green: Double, blue: Double) {
-    let percentKelvin = temperature / 100;
+    let percentKelvin = temperature / 100
     let red, green, blue: Double
 
-    red = clamp(percentKelvin <= 66 ? 255 : (329.698727446 * pow(percentKelvin - 60, -0.1332047592)));
-    green = clamp(percentKelvin <= 66 ? (99.4708025861 * log(percentKelvin) - 161.1195681661) : 288.1221695283 * pow(percentKelvin - 60, -0.0755148492));
-    blue = clamp(percentKelvin >= 66 ? 255 : (percentKelvin <= 19 ? 0 : 138.5177312231 * log(percentKelvin - 10) - 305.0447927307));
+    red = clamp(percentKelvin <= 66 ? 255 : (329.698727446 * pow(percentKelvin - 60, -0.1332047592)))
+    green = clamp(percentKelvin <= 66 ? (99.4708025861 * log(percentKelvin) - 161.1195681661) : 288.1221695283 * pow(percentKelvin - 60, -0.0755148492))
+    blue = clamp(percentKelvin >= 66 ? 255 : (percentKelvin <= 19 ? 0 : 138.5177312231 * log(percentKelvin - 10) - 305.0447927307))
 
     return (red: red / 255, green: green / 255, blue: blue / 255)
 
-    func clamp(_ value: Double) -> Double
-    {
-        return value > 255 ? 255 : (value < 0 ? 0 : value);
+    func clamp(_ value: Double) -> Double {
+        return value > 255 ? 255 : (value < 0 ? 0 : value)
     }
 }
