0eb080529b59224186fef2c2bd1b8e37d0c6aee9
Brendan Thompson <github@blt.is>
2023-08-04 15:53:31 +1000

    feat: add codable conformance for Color

--- Sources/Color.swift
+++ Sources/Color.swift
@@ -61,6 +61,71 @@ public extension Color {
     }
 }
 
+#if os(iOS)
+import UIKit
+#elseif os(watchOS)
+import WatchKit
+#elseif os(macOS)
+import AppKit
+#endif
+
+@available(iOS 14, tvOS 14, macOS 13, *)
+private extension Color {
+#if os(macOS)
+    typealias SystemColor = NSColor
+#else
+    typealias SystemColor = UIColor
+#endif
+
+    var colorComponents: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+
+#if os(macOS)
+        SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
+            // Note that non RGB color will raise an exception, that I don't now how to catch because it is an Objc exception.
+#else
+        guard SystemColor(self).getRed(&r, green: &g, blue: &b, alpha: &a) else {
+                // Pay attention that the color should be convertible into RGB format
+                // Colors using hue, saturation and brightness won't work
+            return nil
+        }
+#endif
+
+        return (r, g, b, a)
+    }
+}
+
+@available(iOS 14, tvOS 14, macOS 13, *)
+extension Color: Codable {
+    enum CodingKeys: String, CodingKey {
+        case red, green, blue
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let r = try container.decode(Double.self, forKey: .red)
+        let g = try container.decode(Double.self, forKey: .green)
+        let b = try container.decode(Double.self, forKey: .blue)
+
+        self.init(red: r, green: g, blue: b)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        guard let colorComponents = colorComponents else {
+            return
+        }
+
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(colorComponents.red, forKey: .red)
+        try container.encode(colorComponents.green, forKey: .green)
+        try container.encode(colorComponents.blue, forKey: .blue)
+    }
+}
+
 // Algorithm taken from Tanner Helland's post: http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
 func componentsForColorTemperature(temperature: Kelvin) -> (red: Double, green: Double, blue: Double) {
     let percentKelvin = temperature / 100
